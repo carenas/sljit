c5d1355559d3482d130935c3374c3e5ba41bcc9c
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2020-08-06 13:25:50 -0700

    macos: enable fastcall function calls
    might had been accidentally disabled, because for a while,
    macOS used to have an ancient (pre GPL3) gcc compiler that
    might had lacked the necessary support.
    
    that problem is likely covered with the recently added check,
    and current "Apple LLVM" doesn't fail to build and seem to
    work with it enabled.
    
    tested with 10.3.6 (which is also the last version that supports
    X86_32 bit applications) but could do with further testing.

--- sljit_src/sljitConfigInternal.h
+++ sljit_src/sljitConfigInternal.h
@@ -556,7 +556,7 @@ typedef double sljit_f64;
 
 #define SLJIT_FUNC
 
-#elif defined(__GNUC__) && !defined(__APPLE__)
+#elif defined(__GNUC__)
 
 #if __GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 4)
 #define SLJIT_FUNC __attribute__ ((fastcall))
